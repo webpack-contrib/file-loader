f46872080cb6df29a7bdce7e4d80c13186197d54
Tobias Koppers <tobias.koppers@googlemail.com>
2013-01-28 15:04:13 +0100

    webpack 0.9

--- README.md
+++ README.md
@@ -7,19 +7,27 @@ var url = require("file!./file.png");
 // => emits file.png as file in the output directory and returns the public url
 ```
 
-If you want to have a custom extension on your file, I have some prepared:
+The filename is the md5 hash of the file. The extension of the required resource is appended.
+
+If you want to have custom pre- and postfix of your file:
 
 ``` javascript
-require("file/js!./javascript.js");
-require("file/html!./page.html");
-require("file/txt!./flash.txt");
-require("file/png!./image.png");
-require("file/jpg!./image.jpg");
-require("file/jpeg!./image.jpeg");
-require("file/swf!./flash.swf");
-```
+require("file?prefix=js/&postfix=.script.js!./javascript.js");
+// => js/0dcbbaa701328a3c262cfd45869e351f.script.js
 
-Look at the source to write your own. Pre- and postfix is possible.
+require("file?prefix=html-&size=6!./page.html");
+// => html-109fa8.html
+
+require("file?postfix!./flash.txt");
+// => c31e9820c001c9c4a86bce33ce43b679
+
+require("file?hash=sha512!./image.png");
+// => a720b9f140d13...781f1f78344.png
+// use sha512 hash instead of md5
+
+require("file?name=picture.png!./myself.png");
+// => picture.png
+```
 
 ## License
 
--- index.js
+++ index.js
@@ -2,18 +2,32 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-module.exports = function(postfix, prefix) {
-	postfix = postfix || "";
-	prefix = prefix || "";
-	return function(content) {
-		this.cacheable && this.cacheable();
-		if(!this.emitFile) throw new Error("emitFile is required from module system");
-		if(this.buffers) content = this.buffers[0];
-		hash = new (require("crypto").Hash)("md5");
+var loaderUtils = require("loader-utils");
+module.exports = function(content) {
+	this.cacheable && this.cacheable();
+	if(!this.emitFile) throw new Error("emitFile is required from module system");
+	var query = loaderUtils.parseQuery(this.query);
+	var url;
+	if(query.name) {
+		url = query.name;
+	} else {
+		var prefix = query.prefix || "";
+		var postfix = query.postfix;
+		if(typeof postfix != "string") {
+			if(this.resource && typeof postfix != "boolean") {
+				var idx = this.resource.lastIndexOf(".");
+				if(idx > 0) postfix = this.resource.substr(idx);
+				else postfix = "";
+			} else postfix = "";
+		}
+		var digest = query.hash || "md5";
+		var digestSize = query.size || 9999;
+		hash = new (require("crypto").Hash)(digest);
 		hash.update(content);
-		var hash = hash.digest("hex");
-		var url = prefix + hash + postfix;
-		this.emitFile(url, content);
-		return "module.exports = " + require("webpack/api/getPublicPrefix") + " + " + JSON.stringify(url);
+		var hash = hash.digest("hex").substr(0, digestSize);
+		url = prefix + hash + postfix;
 	}
+	this.emitFile(url, content);
+	return "module.exports = __webpack_public_path__ + " + JSON.stringify(url);
 }
+module.exports.raw = true;
