1a9a1ce49af9f7c80dd7d620eec94d4bdedbac8b
Bit Planets <bitplanets@gmail.com>
2014-12-05 14:59:55 +0000

    When used with enhanced-require on node.js it throws an error because emitFile is not existent. Fix #2

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ var path = require("path");
 
 module.exports = function(content) {
 	this.cacheable && this.cacheable();
-	if(!this.emitFile) throw new Error("emitFile is required from module system");
 	var query = loaderUtils.parseQuery(this.query);
 	var url = loaderUtils.interpolateName(this, query.name || "[hash].[ext]", {
 		context: query.context || this.options.context,
 		content: content,
 		regExp: query.regExp
 	});
-	this.emitFile(url, content);
+    // When used with enhanced-require on node.js it throws an error because
+    // emitFile is not existent.
+	this.emitFile && this.emitFile(url, content);
 	return "module.exports = __webpack_public_path__ + " + JSON.stringify(url);
 }
 module.exports.raw = true;
