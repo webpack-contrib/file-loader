49cad1334059800e793c86d7eaa4d86831db589d
rondonjon <rondonjon@users.noreply.github.com>
2015-07-15 01:12:35 +0200

    adding missing semicolon
    the missing semicolon yields warning in lint tools.

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ module.exports = function(content) {
 		regExp: query.regExp
 	});
 	this.emitFile(url, content);
-	return "module.exports = __webpack_public_path__ + " + JSON.stringify(url);
+	return "module.exports = __webpack_public_path__ + " + JSON.stringify(url) + ";";
 }
 module.exports.raw = true;
