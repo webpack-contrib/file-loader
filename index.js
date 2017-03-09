567e2c321ddca2d37fd54dfbc57cdafa1e530623
Adrian Miranda <adriancmiranda@gmail.com>
2017-03-09 11:31:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
+var path = require("path");
 var loaderUtils = require("loader-utils");
 
 module.exports = function(content) {
@@ -11,6 +12,10 @@ module.exports = function(content) {
 	var query = loaderUtils.getOptions(this) || {};
 	var configKey = query.config || "fileLoader";
 	var options = this.options[configKey] || {};
+	var filePath = this.resourcePath;
+	var issuerContext = this._module.issuer.context;
+	var relativeUrl = path.relative(issuerContext, filePath).split(path.sep).join("/");
+	var relativePath = relativeUrl && path.dirname(relativeUrl) + "/";
 
 	var config = {
 		publicPath: false,
@@ -27,7 +32,7 @@ module.exports = function(content) {
 		config[attr] = query[attr];
 	});
 
-	var url = loaderUtils.interpolateName(this, config.name, {
+	var url = relativePath + loaderUtils.interpolateName(this, config.name, {
 		context: config.context || this.options.context,
 		content: content,
 		regExp: config.regExp
@@ -38,18 +43,20 @@ module.exports = function(content) {
 	var publicPath = "__webpack_public_path__ + " + JSON.stringify(url);
 
 	if (config.outputPath) {
-		 // support functions as outputPath to generate them dynamically
-		 outputPath = typeof config.outputPath === "function"
-		 ? config.outputPath(url)
-		 : config.outputPath + url
+		// support functions as outputPath to generate them dynamically
+		outputPath = (
+			typeof config.outputPath === "function"
+			? config.outputPath(url)
+			: config.outputPath
+		);
 	}
 
 	if (config.publicPath) {
 		// support functions as publicPath to generate them dynamically
 		publicPath = JSON.stringify(
-				typeof config.publicPath === "function"
-				 ? config.publicPath(url)
-				 : config.publicPath + url
+			typeof config.publicPath === "function"
+			? config.publicPath(url)
+			: config.publicPath + url
 		);
 	}
 
@@ -58,5 +65,6 @@ module.exports = function(content) {
 	}
 
 	return "module.exports = " + publicPath + ";";
-}
+};
+
 module.exports.raw = true;
