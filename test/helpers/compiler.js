7b48231ad2c5eab69b2f028aa9847f96a19951c2
Joshua Wiens <joshuaw@easymetrics.com>
2017-12-15 19:06:11 -0600

    chore: Updates to Defaults 2.0
     - See https://github.com/webpack-contrib/webpack-defaults/releases/tag/v2.0.0-alpha.0

--- test/helpers/compiler.js
+++ test/helpers/compiler.js
@@ -10,38 +10,40 @@ import MemoryFS from 'memory-fs';
 
 const module = (config) => {
   return {
-    rules: config.rules || config.loader
-      ? [
-        {
-          test: config.loader.test || /\.txt$/,
-          use: {
-            loader: path.resolve(__dirname, '../../src'),
-            options: config.loader.options || {},
-          },
-        },
-      ]
-      : [],
+    rules:
+      config.rules || config.loader
+        ? [
+            {
+              test: config.loader.test || /\.txt$/,
+              use: {
+                loader: path.resolve(__dirname, '../../src'),
+                options: config.loader.options || {},
+              },
+            },
+          ]
+        : [],
   };
 };
 
-const plugins = config => ([
-  new webpack.optimize.CommonsChunkPlugin({
-    name: ['runtime'],
-    minChunks: Infinity,
-  }),
-].concat(config.plugins || []));
+const plugins = (config) =>
+  [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['runtime'],
+      minChunks: Infinity,
+    }),
+  ].concat(config.plugins || []);
 
 const output = (config) => {
   return {
     path: path.resolve(
       __dirname,
-      `../outputs/${config.output ? config.output : ''}`,
+      `../outputs/${config.output ? config.output : ''}`
     ),
     filename: '[name].bundle.js',
   };
 };
 
-export default function (fixture, config, options) {
+export default function(fixture, config, options) {
   // webpack Config
   config = {
     devtool: config.devtool || 'sourcemap',
@@ -60,9 +62,11 @@ export default function (fixture, config, options) {
 
   if (!options.output) compiler.outputFileSystem = new MemoryFS();
 
-  return new Promise((resolve, reject) => compiler.run((err, stats) => {
-    if (err) reject(err);
+  return new Promise((resolve, reject) =>
+    compiler.run((err, stats) => {
+      if (err) reject(err);
 
-    resolve(stats);
-  }));
+      resolve(stats);
+    })
+  );
 }
