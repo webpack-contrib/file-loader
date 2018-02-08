fb27d852b7dbb483a48ea059cb6829be6b52dfd3
Claudiomar <claudiomar@ADRIANIZEVIANA.local>
2018-02-08 10:58:07 -0300

    test: add new test to publicPath test with two parameters.

--- test/options/publicPath.test.js
+++ test/options/publicPath.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable
   prefer-destructuring,
 */
+import path from 'path';
 import webpack from '../helpers/compiler';
 
 describe('Options', () => {
@@ -28,6 +29,24 @@ describe('Options', () => {
           options: {
             publicPath(url) {
               return `test/${url}`;
+            },
+          },
+        },
+      };
+
+      const stats = await webpack('fixture.js', config);
+      const { source } = stats.toJson().modules[1];
+
+      expect(source).toMatchSnapshot();
+    });
+
+    test('{Function with ressource param}', async () => {
+      const config = {
+        loader: {
+          test: /(png|jpg|svg)/,
+          options: {
+            publicPath(url, ressource) {
+              return ressource === path.resolve(__dirname, '../fixtures/fixture.js') ? `test/${url}` : `test/${url}`;
             },
           },
         },
