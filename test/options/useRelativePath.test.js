3b071f5279ef6cb912d66560e38aa9bbe7214c30
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2018-02-20 22:11:51 +0300

    fix(index): `context` takes precedence over `issuer.context` (`options.useRelativePath`)

--- test/options/useRelativePath.test.js
+++ test/options/useRelativePath.test.js
@@ -5,37 +5,70 @@ import webpack from '../helpers/compiler';
 
 describe('Options', () => {
   describe('useRelativePath', () => {
-    test('this.options.context', async () => {
+    test('`false`', async () => {
       const config = {
         loader: {
           test: /(png|jpg|svg)/,
           options: {
-            useRelativePath: true,
+            useRelativePath: false,
           },
         },
       };
 
-      const stats = await webpack('fixture.js', config);
-      const { source } = stats.toJson().modules[1];
+      const stats = await webpack('fixture-nested.js', config);
+      const { assets, source } = stats.toJson().modules[1];
 
-      expect(source).toMatchSnapshot();
+      expect({ assets, source }).toMatchSnapshot();
     });
 
-    test('options.context', async () => {
+    test('`true`', async () => {
       const config = {
         loader: {
           test: /(png|jpg|svg)/,
           options: {
-            context: '/relative/',
             useRelativePath: true,
           },
         },
       };
 
-      const stats = await webpack('fixture.js', config);
-      const { source } = stats.toJson().modules[1];
+      const stats = await webpack('fixture-nested.js', config);
+      const { assets, source } = stats.toJson().modules[1];
 
-      expect(source).toMatchSnapshot();
+      expect({ assets, source }).toMatchSnapshot();
+    });
+
+    test('`true` with relative `context`', async () => {
+      const config = {
+        loader: {
+          test: /(png|jpg|svg)/,
+          options: {
+            context: './test/fixtures/nested/',
+            useRelativePath: true,
+          },
+        },
+      };
+
+      const stats = await webpack('fixture-nested.js', config);
+      const { assets, source } = stats.toJson().modules[1];
+
+      expect({ assets, source }).toMatchSnapshot();
+    });
+
+    test('`true` with absolute `context`', async () => {
+      const config = {
+        loader: {
+          test: /(png|jpg|svg)/,
+          options: {
+            context: '../nested/',
+            useRelativePath: true,
+          },
+        },
+      };
+
+      const stats = await webpack('fixture-nested.js', config);
+      const { assets, source } = stats.toJson().modules[1];
+
+      expect({ assets, source }).toMatchSnapshot();
     });
   });
 });
