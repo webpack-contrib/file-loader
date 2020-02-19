cd8698b1d9fd560d85e912acca9a1e24f00e18f8
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2020-02-19 20:57:14 +0300

    feat: support the `query` template for the `name` option (#366)

--- test/name-option.test.js
+++ test/name-option.test.js
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import {
   compile,
   execute,
@@ -22,7 +24,7 @@ describe('"name" option', () => {
 
   it('should work with "String" value', async () => {
     const compiler = getCompiler('simple.js', {
-      name: '[hash].string.[ext]',
+      name: '[hash].string.[ext][query]',
     });
     const stats = await compile(compiler);
 
@@ -37,10 +39,39 @@ describe('"name" option', () => {
 
   it('should work with "Function" value', async () => {
     const compiler = getCompiler('simple.js', {
-      name() {
-        return '[hash].function.[ext]';
+      name(resourcePath, resourceQuery) {
+        expect(resourcePath).toBeDefined();
+        expect(resourceQuery).toBeDefined();
+
+        return '[hash].function.[ext][query]';
       },
     });
+    const stats = await compile(compiler);
+
+    expect(
+      execute(readAsset('main.bundle.js', compiler, stats))
+    ).toMatchSnapshot('result');
+    expect(normalizeErrors(stats.compilation.warnings)).toMatchSnapshot(
+      'warnings'
+    );
+    expect(normalizeErrors(stats.compilation.errors)).toMatchSnapshot('errors');
+  });
+
+  it('should work for CDN support query params', async () => {
+    const compiler = getCompiler(
+      'cdn.js',
+      {
+        name: '[path][name].[ext][query]',
+      },
+      {
+        output: {
+          path: path.resolve(__dirname, './outputs'),
+          publicPath: 'https://cdn.example.com/',
+          filename: '[name].bundle.js',
+          chunkFilename: '[name].chunk.js',
+        },
+      }
+    );
     const stats = await compile(compiler);
 
     expect(
